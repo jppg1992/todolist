4496edbe4d33ce04463b13c728d061efef19d1b1
João paulo P. Godinho <j_paulo_g@hotmail.com>
2023-10-14 16:31:04 -0300

    finalizando o projeto para deploy

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:latest as build
+
+RUN apt-get update
+RUN apt-get install openjdk-17-jdk -y
+
+COPY . .
+
+RUN apt-get install maven -y
+RUN mvn clean install
+
+EXPOSE 8080
+
+COPY --from=build /target/todolist-0.0.1.jar app.jar
+
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/br/com/joaogodinho/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,63 @@
+package br.com.joaogodinho.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.joaogodinho.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    var servletPath = request.getServletPath();
+
+    if (servletPath.startsWith("/tasks")) {
+
+      // pegar autenticação (usuario e senha)
+      var authorization = request.getHeader("Authorization");
+      // retira o tipo de authorização
+      var authEncoded = authorization.substring("Basic".length()).trim();
+      // decodifica os dados da autenticação
+      byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+      var authString = new String(authDecoded);
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
+
+      // validar user
+      var user = this.userRepository.findByUsername(username);
+      if (user == null) {
+        response.sendError(401);
+      } else {
+        // validar senha
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+        if (passwordVerify.verified) {
+          request.setAttribute("idUser", user.getId());
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401);
+        }
+
+      }
+
+    } else {
+      filterChain.doFilter(request, response);
+    }
+  }
+
+}
